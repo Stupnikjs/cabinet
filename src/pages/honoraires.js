13650bd2ec840842c25a2c7e57e97dc2cbf8778f
Stupnikjs <n.boudier.ph@gmail.com>
2022-11-21 11:47:30 +0100

    dernier arragement avant partage axel

--- src/pages/honoraires.js
+++ src/pages/honoraires.js
@@ -9,7 +9,10 @@ const honoraires = () => {
     return (
         <main  className={classhonoraires}>
         <Header  className={honorairesheader}><h1>Honoraires</h1></Header>
-        <Section></Section>
+        <Section> <h4>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapient
+            e, odit perferendis. Eum voluptate distinctio doloribus quaerat nisi quas hic iure,
+             aliquam ut est, dolores fugiat impedit delectus cum</h4>que minus labore?
+             </Section>
         <Footer></Footer>
     </main>
     );

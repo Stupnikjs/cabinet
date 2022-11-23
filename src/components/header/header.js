ffa19d01ad4d4ed7d4fe86cb269c0fa155f6f3f7
Stupnikjs <n.boudier.ph@gmail.com>
2022-11-23 09:41:00 +0100

    safari pblm

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,14 +4,14 @@ import Title from './title';
 import {mtop10, mtop20} from "./title.module.css"; 
 import {backicon, nobackicon} from "./header.module.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDoorOpen } from '@fortawesome/free-solid-svg-icons'; 
+import { faHouseChimney} from '@fortawesome/free-solid-svg-icons'; 
 import { Link } from 'gatsby';
 
 const Header = ({main, children, className, margintop}) => {
     console.log(main)
     return (
         <header className={className}>
-            <Link to={"/"}><FontAwesomeIcon className={ main? nobackicon: backicon} icon={faDoorOpen}/></Link>
+            <Link to={"/"}><FontAwesomeIcon className={ main? nobackicon: backicon} icon={faHouseChimney}/></Link>
             <Navbar main={main}></Navbar>
             <Title className={margintop === "20" ? mtop20: mtop10}>{children}</Title>
         </header>
